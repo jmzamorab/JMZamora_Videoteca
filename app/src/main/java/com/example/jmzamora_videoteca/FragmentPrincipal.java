4fbb06d95a99d0ba0e2bc352c59fead3e9e44670
jmzamorab <josem.zamorab@gmail.com>
2017-07-05 21:10:09 +0200

    Crear categorías y tarjetas personalizadas

--- app/src/main/java/com/example/jmzamora_videoteca/FragmentPrincipal.java
+++ app/src/main/java/com/example/jmzamora_videoteca/FragmentPrincipal.java
@@ -1,12 +1,17 @@
 package com.example.jmzamora_videoteca;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
 import android.support.v17.leanback.widget.HeaderItem;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.Presenter;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,9 +20,11 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.jmzamora_videoteca.R.drawable.movie;
-
 public class FragmentPrincipal extends BrowseFragment {
+    private static final int GRID_ITEM_WIDTH = 200;
+    private static final int GRID_ITEM_HEIGHT = 200;
+
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -58,6 +65,7 @@ public class FragmentPrincipal extends BrowseFragment {
                 rowsAdapter.add(new ListRow(header, listRowAdapter));
             }
         }
+        listaPreferencias(rowsAdapter);
         setAdapter(rowsAdapter);
     }
 
@@ -71,5 +79,38 @@ public class FragmentPrincipal extends BrowseFragment {
             }
         }
         return categories;
+    }
+
+    private void listaPreferencias(ArrayObjectAdapter adapter) {
+        HeaderItem gridHeader = new HeaderItem(adapter.size() - 1, "PREFERENCIAS");
+        GridItemPresenter mGridPresenter = new GridItemPresenter();
+        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+        gridRowAdapter.add("Vistas");
+        gridRowAdapter.add("Errores");
+        gridRowAdapter.add("Preferencias");
+        adapter.add(new ListRow(gridHeader, gridRowAdapter));
+    }
+
+    private class GridItemPresenter extends Presenter {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setBackgroundColor(getResources().getColor(R.color.default_background));
+            view.setTextColor(Color.WHITE);
+            view.setGravity(Gravity.CENTER);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+            ((TextView) viewHolder.view).setText((String) item);
+        }
+
+        @Override
+        public void onUnbindViewHolder(ViewHolder viewHolder) {
+        }
     }
 }
