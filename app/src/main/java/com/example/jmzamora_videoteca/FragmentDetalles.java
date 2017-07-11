84ae64eb70d0288ef1b7da184f904622a352c741
jmzamorab <josem.zamorab@gmail.com>
2017-07-11 20:23:55 +0200

    Hasta ver trailer funcionando

--- app/src/main/java/com/example/jmzamora_videoteca/FragmentDetalles.java
+++ app/src/main/java/com/example/jmzamora_videoteca/FragmentDetalles.java
@@ -1,8 +1,17 @@
 package com.example.jmzamora_videoteca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BackgroundManager;
 import android.support.v17.leanback.app.DetailsFragment;
+import android.support.v17.leanback.widget.Action;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.ClassPresenterSelector;
+import android.support.v17.leanback.widget.DetailsOverviewRow;
+import android.support.v17.leanback.widget.DetailsOverviewRowPresenter;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.OnActionClickedListener;
 import android.util.DisplayMetrics;
 
 import com.bumptech.glide.Glide;
@@ -11,9 +20,7 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
 
-/**
- * Created by padres on 05/07/2017.
- */
+/*VOY POR PAGINA 21, abrir tambien PYTO AndroidTvApp*/
 
 public class FragmentDetalles extends DetailsFragment {
     private Movie mSelectedMovie;
@@ -22,12 +29,38 @@ public class FragmentDetalles extends DetailsFragment {
     private static final String MOVIE = "Movie";
     private BackgroundManager mBackgroundManager;
 
+    private static final int ACTION_WATCH_TRAILER = 1;
+    private static final int DETAIL_THUMB_WIDTH = 274;
+    private static final int DETAIL_THUMB_HEIGHT = 274;
+    private DetailsOverviewRowPresenter mDorPresenter;
+    private static final int NUM_COLS = 10;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initBackground();
         mSelectedMovie = (Movie) getActivity().getIntent().getSerializableExtra(MOVIE);
         updateBackground(mSelectedMovie.getBackgroundImageURI().toString());
+        mDorPresenter = new DetailsOverviewRowPresenter(new DetailsDescriptionPresenter());
+        mMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+        cargarDetalles(mSelectedMovie);
+        mDorPresenter.setSharedElementEnterTransition(getActivity(), ActividadDetalles.SHARED_ELEMENT_NAME);
+
+        mDorPresenter.setOnActionClickedListener(new OnActionClickedListener() {
+            @Override
+            public void onActionClicked(Action action) {
+                if (action.getId() == ACTION_WATCH_TRAILER) {
+                    Intent intent = new Intent(getActivity(),
+                            PlaybackOverlayActivity.class);
+                    intent.putExtra(getResources().getString(R.string.movie),
+                            mSelectedMovie);
+                    intent.putExtra(getResources().getString(R.string.should_start),
+                            true);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     private void initBackground() {
@@ -44,5 +77,24 @@ public class FragmentDetalles extends DetailsFragment {
                 mBackgroundManager.setDrawable(resource);
             }
         });
+    }
+
+    private void cargarDetalles(Movie mSelectedMovie) {
+        final DetailsOverviewRow row = new DetailsOverviewRow(mSelectedMovie);
+        int width = Utils.convertDpToPixel(getActivity().getApplicationContext(), DETAIL_THUMB_WIDTH);
+        int height = Utils.convertDpToPixel(getActivity().getApplicationContext(), DETAIL_THUMB_HEIGHT);
+        Glide.with(getActivity()).load(mSelectedMovie.getCardImageUrl()).centerCrop().into(new SimpleTarget<GlideDrawable>(width, height) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                row.setImageDrawable(resource);
+            }
+        });
+        row.addAction(new Action(ACTION_WATCH_TRAILER, "VER", "TRAILER"));
+        ClassPresenterSelector ps = new ClassPresenterSelector();
+        ps.addClassPresenter(DetailsOverviewRow.class, mDorPresenter);
+        ps.addClassPresenter(ListRow.class, new ListRowPresenter());
+        ArrayObjectAdapter adapter = new ArrayObjectAdapter(ps);
+        adapter.add(row);
+        setAdapter(adapter);
     }
 }
